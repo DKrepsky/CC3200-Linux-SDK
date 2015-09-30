c2543fe52e09c2b164943a81ac973011930782fb
David Krepsky <david@akenge.com>
2015-09-30 13:58:45 -0300

    work done

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # CC3200-Linux-SDK
 
+## NOTE: Since TI provides now support for CC3200 developmnent under linux with CCS, this repository won't receive any updates.
+
 ### Overview
 This repo contains a reworked SDK for the CC3200 microcontroller, from Texas 
 Instruments, to work under linux.
--- examples/Blinky/Debug/startup_gcc.d
+++
@@ -1,7 +0,0 @@
-startup_gcc.o: ../startup_gcc.c \
- /opt/cc3200/eclipse//../CC3200-Linux-SDK/src/inc/hw_nvic.h \
- /opt/cc3200/eclipse//../CC3200-Linux-SDK/src/inc/hw_types.h
-
-/opt/cc3200/eclipse//../CC3200-Linux-SDK/src/inc/hw_nvic.h:
-
-/opt/cc3200/eclipse//../CC3200-Linux-SDK/src/inc/hw_types.h:
